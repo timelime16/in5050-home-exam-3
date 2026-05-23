0e77a1b9b8d1a78a07b409eb9402cd536be290e7
timelime16 <limmit1661@gmail.com>
2026-05-23 21:29:58 +0200

    bugfix

--- c63-in-c/me.h
+++ c63-in-c/me.h
@@ -6,6 +6,6 @@
 // Declaration
 void c63_motion_estimate(struct c63_common *cm, int start_mb_row, int end_mb_row);
 
-void c63_motion_compensate(struct c63_common *cm);
+void c63_motion_compensate(struct c63_common *cm, int start_mb_row, int end_mb_row);
 
 #endif  /* C63_ME_H_ */
